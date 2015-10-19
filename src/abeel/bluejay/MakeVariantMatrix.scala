69a02cf81678f160d9821aef74c469c7cd3365e9
Arlin <arlinkeo@gmail.com>
2015-10-19 21:01:03 +0200

    Read only snps when configured

--- src/abeel/bluejay/MakeVariantMatrix.scala
+++ src/abeel/bluejay/MakeVariantMatrix.scala
@@ -9,12 +9,12 @@ import atk.util.TimeInterval
 
 object MakeVariantMatrix extends Tool {
 
- case class Config(vcf: File = null, outputPrefix: String = "", snpsOnly: Boolean = false)
+  case class Config(vcf: File = null, outputPrefix: String = "", snpsOnly: Boolean = false)
 
   def main(args: Array[String]): Unit = {
     val parser = new scopt.OptionParser[Config]("java -jar bluejay.jar variant-matrix") {
       opt[File]('v', "vcf") required () action { (x, c) => c.copy(vcf = x) } text ("File containing a list of VCF files")
-      opt[String]('o',"output") action{ (x, c) => c.copy(outputPrefix = x) } text ("Output prefix")
+      opt[String]('o', "output") action { (x, c) => c.copy(outputPrefix = x) } text ("Output prefix")
       opt[Unit]("snps-only") action { (x, c) => c.copy(snpsOnly = true) } text ("Use only SNPs to make variant matrix.")
     }
 
@@ -24,12 +24,12 @@ object MakeVariantMatrix extends Tool {
   }
 
   def matrixify(config: Config) {
-    val files = tLines(config.vcf).map(f=>new File(f))
+    val files = tLines(config.vcf).map(f => new File(f))
 
     val listOfVariants = files.foldLeft(Set[String]())((r, c) => r ++ {
       println("Pre-processing " + c)
       val lines = VCFFile(c)
-      
+
       if (config.snpsOnly) lines.filter(_.pass).filter(vl => vl.refLength == 1 && vl.altLength == 1).map(vl => vl.pos + "_" + vl.ref + "_" + vl.alt)
       else lines.filter(_.pass).map(vl => vl.pos + "_" + vl.ref + "_" + vl.alt)
 
@@ -43,19 +43,20 @@ object MakeVariantMatrix extends Tool {
     pwx.close
 
     val variants = tLines(config.outputPrefix + "variantlist.txt")
-  
+
     val pw = new PrintWriter(config.outputPrefix + "variantmatrix.txt")
     pw.println("$$\t" + variants.mkString("\t"))
-    var idx=0
-    val startTime=System.currentTimeMillis()
+    var idx = 0
+    val startTime = System.currentTimeMillis()
     files.map(f => {
-      idx+=1
-      val elapsed=System.currentTimeMillis()-startTime+1.0
-      println("Processing: " + f+ "\n\t"+idx+"/"+files.size+"\t"+new TimeInterval(elapsed.toLong)+"\tETA: "+new TimeInterval((((elapsed/idx)*files.size)-elapsed).toLong))
+      idx += 1
+      val elapsed = System.currentTimeMillis() - startTime + 1.0
+      println("Processing: " + f + "\n\t" + idx + "/" + files.size + "\t" + new TimeInterval(elapsed.toLong) + "\tETA: " + new TimeInterval((((elapsed / idx) * files.size) - elapsed).toLong))
       val lines = VCFFile(f)
       val g = f.toString()
       val arr = Array.ofDim[Int](variants.length)
-      lines.toList.filter(_.pass).map(vl => {
+      val listOfVars = if (config.snpsOnly) lines.toList.filter(_.pass).filter(vl => vl.refLength == 1 && vl.altLength == 1) else lines.toList.filter(_.pass)
+      listOfVars.map(vl => {
         val id = vl.pos + "_" + vl.ref + "_" + vl.alt
         val idx = variants.indexOf(id)
         arr(idx) = 1
